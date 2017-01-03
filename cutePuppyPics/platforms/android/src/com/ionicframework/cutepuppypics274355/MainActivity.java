67e8f4ab75f141bcf38c8e56cab2fb7970f192fe
Viktor Benei <viktor.benei@gmail.com>
2017-01-03 22:41:46 +0100

    regen

--- cutePuppyPics/platforms/android/src/com/ionicframework/cutepuppypics274355/MainActivity.java
+++ cutePuppyPics/platforms/android/src/com/ionicframework/cutepuppypics274355/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.ionicframework.cutepuppypics811288;
+package com.ionicframework.cutepuppypics274355;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
